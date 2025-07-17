62555209dc4315158387af7f03a0e7d3e6ec720a
tshays <tshayshawel@gmail.com>
2025-07-17 14:59:28 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,13 +10,21 @@ export default {
 	],
 	prefix: "",
 	theme: {
-		container: {
-			center: true,
-			padding: '2rem',
-			screens: {
-				'2xl': '1400px'
-			}
-		},
+  container: {
+    center: true,
+    padding: {
+      DEFAULT: '1rem',
+      sm: '1.5rem',
+      lg: '2rem',
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1400px',
+    }
+  },
 		extend: {
 			colors: {
 				border: 'hsl(var(--border))',
